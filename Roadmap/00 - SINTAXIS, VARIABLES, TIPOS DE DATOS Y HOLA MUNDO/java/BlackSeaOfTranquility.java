a5058057b493df51d7ba723992c0f2710ca505f9
BlackSeaOfTranquility <151750365+BlackSeaOfTranquility@users.noreply.github.com>
2024-05-10 19:16:00 +0200

    Add files via upload
    "#00 - Sintaxis, Variables, Tipos de Datos y Hola Mundo"
    
    Hola a todos. Soy BlackSeaOfTranquility y esta es mi primera práctica en GitHub con el curso de Lógica de Programación. Quiero practicar y mejorar en Java. 
    Si algo está mal en lo que voy subiendo y cambiando, no dudéis en corregirme.
    Un saludo a todos.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/BlackSeaOfTranquility.java
@@ -0,0 +1,35 @@
+/**
+ * COMENTARIO Para párrafos.
+ */
+
+// COMENTARIO Para líneas.
+
+// ENLACE: https://docs.oracle.com/javase/tutorial/java/TOC.html
+
+package clase0; //SINTAXIS: Esto indica que el paquete de Java es "clase0".
+
+public class BlackSeaOfTranquility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Darle valor a una variable.
+		String variable = "Mi variable";
+		
+		//Una constante.
+		final int dias = 7;
+		
+		//Tipos de datos primitivos del lenguaje
+		int var = 5;
+		double var = 5.5;
+		float var = 5.5;
+		boolean var = true;
+		boolean var = false;
+		char var = 'J';
+		byte var = 5;
+		short var = 5;
+		long var = 1000L;
+		
+		System.out.println("Hola, Java!"); //Imprimir "Hola, Java!" en la terminal.
+	}
+
+}
